b8e387e58bd87b9f5acf0c16f0d078b8e668b7a6
Lynne Fuyuna <yryuu@outlook.com>
2023-11-19 22:20:41 +0800

    fix: missing required attrib

--- pkg/dto/register.go
+++ pkg/dto/register.go
@@ -6,19 +6,19 @@ type RegisterCustomerPayload struct {
 	Email        string `json:"email" validate:"required"`
 	Birthdate    string `json:"birth_date" validate:"required"`
 	Address      string `json:"address" validate:"required"`
-	PhotoProfile string `json:"photo_profile" validate:"required"`
+	PhotoProfile string `json:"photo_profile"`
 	Username     string `json:"username" validate:"required"`
 	Password     string `json:"password" validate:"required"`
 }
 
 type RegisterMerchantPayload struct {
-	LegalName    string `json:"name"`
-	Phone        string `json:"phone"`
-	Address      string `json:"address"`
-	Email        string `json:"email"`
-	PICName      string `json:"pic_name"`
-	PICEmail     string `json:"pic_email"`
-	PICPhone     string `json:"pic_phone"`
+	LegalName    string `json:"name" validate:"required"`
+	Phone        string `json:"phone" validate:"required"`
+	Address      string `json:"address" validate:"required"`
+	Email        string `json:"email" validate:"required"`
+	PICName      string `json:"pic_name" validate:"required"`
+	PICEmail     string `json:"pic_email" validate:"required"`
+	PICPhone     string `json:"pic_phone" validate:"required"`
 	PhotoProfile string `json:"photo_profile"`
 	Username     string `json:"username" validate:"required"`
 	Password     string `json:"password" validate:"required"`
